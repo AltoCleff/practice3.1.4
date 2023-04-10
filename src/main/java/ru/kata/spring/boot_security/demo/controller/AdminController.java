5e736bc284c8c6857ae782be00b29ed141c5b02a
NexusGrid <nexusgrid8@gmail.com>
2023-04-11 00:13:07 +0300

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,50 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasAnyRole('ADMIN')")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userService.allUsers());
+        model.addAttribute("user", new User());
+        return "users";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable("id") long id, Model model) {
+
+        model.addAttribute("user", userService.findUserById(id));
+        return "updateUser";
+    }
+
+    @PostMapping ("/updated")
+    public String update(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -0,0 +1,117 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private String name;
+
+    private String lastName;
+
+    private String email;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
